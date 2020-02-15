e361791ef01d52f300845785cadd1be0dc9a3f0e
lilyrisa <minhmen99@gmail.com>
2020-02-16 02:27:50 +0700

    login register

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
+function getCookies(domain, name, callback) {
+    chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+        if(callback) {
+            callback(cookie);
+        }
+    });
+}
 chrome.runtime.onInstalled.addListener(function() {
-    var newURL = 'login.html';
-    chrome.tabs.create({ url: newURL });
+	getCookies('http://localhost/server-remote-control/tool.php','token', function(cookie){
+		if(cookie != null){
+			
+		}else{
+			var newURL = 'login.html';
+    		chrome.tabs.create({ url: newURL });
+		}
+	});
+    
+
   });
